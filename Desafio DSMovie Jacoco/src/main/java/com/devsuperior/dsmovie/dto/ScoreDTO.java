6fbb0fe39d5b545c026fa90eeb93fb800e5a6236
andrade <viniciusdsandrade0662@gmail.com>
2025-10-15 21:53:46 -0300

    refactor: convert DTO classes to records for improved readability and conciseness

--- Desafio DSMovie Jacoco/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
+++ Desafio DSMovie Jacoco/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
@@ -10,32 +10,28 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
-public class ScoreDTO {
-	
-	private static final DecimalFormat df = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US));
+public record ScoreDTO(
 
-	@NotNull(message = "Required field")
-	private Long movieId;
+        @NotNull(message = "Required field")
+        Long movieId,
 
-	@PositiveOrZero(message = "Score should be greater than or equal to zero")
-	@Max(value = 5, message = "Score should not be greater than five")
-	private Double score;
+        @PositiveOrZero(message = "Score should be greater than or equal to zero")
+        @Max(value = 5, message = "Score should not be greater than five")
+        Double score
+) {
+    private static final DecimalFormat df =
+            new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US));
 
-	public ScoreDTO(Long movieId, Double score) {
-		this.movieId = movieId;
-		this.score = Double.valueOf(df.format(score));
-	}
-	
-	public ScoreDTO(ScoreEntity score) {
-		this.movieId = score.getId().getMovie().getId();
-		this.score = Double.valueOf(df.format(score.getValue()));
-	}
-	
-	public Long getMovieId() {
-		return movieId;
-	}
+    public ScoreDTO {
+        if (score != null) {
+            score = Double.valueOf(df.format(score));
+        }
+    }
 
-	public Double getScore() {
-		return score;
-	}
+    public ScoreDTO(ScoreEntity entity) {
+        this(
+                entity.getId().getMovie().getId(),
+                entity.getValue() == null ? null : Double.valueOf(df.format(entity.getValue()))
+        );
+    }
 }
